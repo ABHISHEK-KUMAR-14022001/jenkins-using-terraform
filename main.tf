1e2f938bc2bf60a4796977b70da75e1fbccecff8
Abhishek Kumar <abhisheksmscollege@gmail.com>
2023-10-16 11:24:26 +0530

    second change

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web" {
   ami           = "ami-053b0d53c279acc90" # Amazon Linux
   instance_type = "t2.micro"
-  key_name     = "jenkin-key"
-  count         = 10
+  key_name     = "terraform-1"
+  
   tags = {
     
-    Name = "Nano-file-${count.index}"
+    Name = "jenkin"
   }
 }
