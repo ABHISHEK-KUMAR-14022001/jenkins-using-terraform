ab3016e3ebaa5c7d7fbeedbd9c4d614d71c939ad
Abhishek kumar <87716036+ABHISHEK-KUMAR-14022001@users.noreply.github.com>
2023-10-19 09:10:33 +0530

    Update ec2-instace.tf
    change

--- ec2-instace.tf
+++ ec2-instace.tf
@@ -10,7 +10,6 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  access_key = "AKIA4GH2WA62T6EO2SVF"
-  secret_key = "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"
   region  = "us-east-1"
+  profile="Default"
 }
